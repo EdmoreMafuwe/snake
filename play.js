34c08affa81d03108e794aa2b89de142eef2ccd3
EdmoreMafuwe <126406684+EdmoreMafuwe@users.noreply.github.com>
2023-12-14 01:08:02 -0800

    Update play.js

--- play.js
+++ play.js
@@ -1,9 +1,5 @@
+const {connect} = require('./client');
+const {setupInput} = require('./input');
 
-
-const { connect } = require('./client');
-const { setupInput } = require('./input');
-console.log('Connecting ...');
+console.log("Connecting ...");
 setupInput(connect());
-
-
-
